d9c3aae592e56b762fbca12321ee046a5d71a156
Josue <josueisahu93@gmail.com>
2016-08-22 15:24:28 -0600

    Todo lo restante

---
+++ VERSIONES/TabHostViewPager2/app/src/main/java/com/example/serviciosocial/tabhostviewpager2/Tab1Fragment.java
@@ -0,0 +1,20 @@
+package com.example.serviciosocial.tabhostviewpager2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab1Fragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		
+		View v = inflater.inflate(R.layout.tab1fragment, container, false);
+		
+		return v;
+	}
+}
